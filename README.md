26e9d7756fc131857b1aaba1eea66ed80b2ffb63
rsninja722 <jnick722547@gmail.com>
2021-03-11 22:36:13 -0500

    looks nicer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RaspberryTris
-tetris for raspberry pi  
+tetris for Raspberry PI  
+
+Uses GPIO inputs to play tetris in the console of the raspbian OS on a Raspberry PI
+
+
+this is the first thing I've made in python, so I expect there are way more efficient ways to do everything
+
+here's a rough idea of what the circuit was like:
+![](diagram.png)
--- tetris.py
+++ tetris.py
@@ -2,9 +2,12 @@ import time
 import curses
 import os
 from random import shuffle
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
+
 
 def main():
+
+    # 7 tetraminos
     pieces = [
         [
             [False, False, False, False],
@@ -45,88 +48,94 @@ def main():
         ]
     ]
 
-    pieceNames = [
-        "i",
-        "o",
-        "j",
-        "l",
-        "s",
-        "z",
-        "t"
-    ]
-    
+    # input pins, and last input states
     leftKey = 22
     leftKeyLast = True
-    
+
     rightKey = 27
     rightKeyLast = True
-    
+
     CWKey = 10
     CWKeyLast = True
-    
+
     CCWKey = 9
     CCWKeyLast = True
-    
+
     dropKey = 11
-    
+
     playing = True
     frame = 0
 
+    # current piece
     piece = []
+    # piece index of upcoming piece
     preview = -1
+    # "bag" for bag randomizer, one of each 7 pieces are put in the bag, then everytime the player needs a new piece, it is taken out of the bag.
+    # When the bag is empty, it is filled again. This is the modern standard for making more fair random piece generation in tetris
     bag = []
 
+    # game grid
     grid = []
 
+    # current piece offset
     curX = 3
     curY = 0
 
+    # delay before piece can be manually dropped again
     dropDelay = 0
 
+    # initialize printing
     stdscr = curses.initscr()
     curses.noecho()
     curses.cbreak()
 
-    
-    GPIO.setwarnings(False) 
-    GPIO.setmode(GPIO.BCM) 
+    # setup GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(rightKey, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(leftKey, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(CWKey, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(CCWKey, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(dropKey, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    # create empty grid
     for i in range(20):
         grid.append([])
         for j in range(10):
             grid[i].append(False)
 
+    # places the current piece in the grid, and tries to clear lines
     def place(matrix):
         for y in range(len(matrix)):
             for x in range(len(matrix)):
                 if matrix[y][x] == True:
                     grid[y + curY][x + curX] = True
         clearLines()
-        
+
+    # clears any complete rows
     def clearLines():
         for y in range(20):
             full = True
+            # stop if row isn't complete
             for x in range(10):
                 if grid[y][x] == False:
                     full = False
                     break
+            # if it is complete, shift above rows down by one
             if full == True:
                 for x in range(10):
                     grid[y][x] = False
-                for y2 in range(y,0,-1):
+                for y2 in range(y, 0, -1):
                     for x in range(10):
                         grid[y2][x] = grid[y2-1][x]
-    
+
+    # refills the bag for randomizing pieces
     def fillBag():
         for i in range(7):
             bag.append(i)
         shuffle(bag)
 
+    # returns a matrix with side length n rotated clockwise
     def rotCW(matrix, n):
         ret = []
         for i in range(n):
@@ -139,6 +148,7 @@ def main():
 
         return ret
 
+    # returns a matrix with side length n rotated counter clockwise
     def rotCCW(matrix, n):
         ret = []
         for i in range(n):
@@ -149,7 +159,8 @@ def main():
             for j in range(n):
                 ret[i][j] = matrix[j][n - i - 1]
         return ret
-    
+
+    # returns true if piece is out of bounds, or touching blocks in the grid
     def colliding():
         for y in range(len(piece)):
             for x in range(len(piece)):
@@ -158,11 +169,17 @@ def main():
                         return True
                     if grid[y + curY][x + curX] == True:
                         return True
+
+    # initialize pieces
     fillBag()
     preview = bag.pop()
+
+    # main game loop
     while playing:
         time.sleep(0.016)
         frame += 1
+
+        # gravity
         if frame % 60 == 0:
             curY += 1
             if colliding():
@@ -173,6 +190,7 @@ def main():
                 curY = 0
                 dropDelay = 20
 
+        # use next piece if current one has been placed
         if len(piece) == 0:
             piece = pieces[preview]
             if len(bag) > 0:
@@ -181,6 +199,7 @@ def main():
                 fillBag()
                 preview = bag.pop()
 
+        # move right
         if GPIO.input(rightKey) == False:
             if rightKeyLast == True:
                 curX += 1
@@ -189,7 +208,8 @@ def main():
                 rightKeyLast = False
         else:
             rightKeyLast = True
-            
+
+        # move left
         if GPIO.input(leftKey) == False:
             if leftKeyLast == True:
                 curX -= 1
@@ -198,7 +218,8 @@ def main():
                 leftKeyLast = False
         else:
             leftKeyLast = True
-            
+
+        # rotate clockwise
         if GPIO.input(CWKey) == False:
             if CWKeyLast == True:
                 piece = rotCW(piece, len(piece))
@@ -207,7 +228,8 @@ def main():
                 CWKeyLast = False
         else:
             CWKeyLast = True
-            
+
+        # rotate counter clockwise
         if GPIO.input(CCWKey) == False:
             if CCWKeyLast == True:
                 piece = rotCCW(piece, len(piece))
@@ -216,7 +238,8 @@ def main():
                 CCWKeyLast = False
         else:
             CCWKeyLast = True
-            
+
+        # drop piece quicker
         if GPIO.input(dropKey) == False and dropDelay <= 0:
             curY += 1
             if colliding():
@@ -228,22 +251,28 @@ def main():
                 dropDelay = 20
         else:
             dropDelay -= 1
-        # print    
+
+        # print game
         lines = []
         for y in range(20):
             lines.append([])
+            # left wall
             lines[y].append("| ")
+            # grid
             for x in range(10):
                 if grid[y][x] == True:
                     lines[y].append("# ")
                 else:
                     lines[y].append("` ")
+            # right wall
             lines[y].append("|")
+        # draw piece
         for y in range(len(piece)):
             for x in range(len(piece)):
                 if piece[y][x]:
                     if y+curY > -1 and y+curY < 20 and (x+curX) + 1 > 0 and (x+curX) + 1 < 11:
                         lines[y+curY][(x+curX) + 1] = "# "
+        # draw preview
         if len(bag) > 0:
             pre = pieces[preview]
             for y in range(len(pre)):
@@ -252,14 +281,17 @@ def main():
                         lines[y+1].append(" #")
                     else:
                         lines[y+1].append("  ")
-                
+
+        # render to console
         for y in range(20):
             line = ""
             for x in lines[y]:
                 line += x
                 stdscr.addstr(y, 0, line + "  ")
         stdscr.refresh()
-    input("lol")
+
+    input("I dont think this does anything but I dont want to delete it")
 
 
+# do the game
 main()
